f91144abc25f93874a08bc03aeee8e43c3dcccde
Guilherme <gui.moreiradasilva27@gmail.com>
2023-03-03 14:19:25 -0300

    Commit Initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on("connection", (socket) => {
+    socket.on("disconnect", () => {
+        console.log("X deu disconnect " + socket.id)
+    })
+
+    socket.on("msg", (data) => {
+        io.emit("showmsg", data)
+        console.log(data)
+    })
+
+    socket.on("name", (data) => {
+        socket.emit("nome", data)
+        console.log(data)
+    })
+
+})
+
+
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/chat", (req, res) => {
+    res.render("chat")
+})
+
+
+http.listen(8080, () => {
+    console.log("Servidor está rodando.")
+})
---
+++ public/js/script.js
@@ -0,0 +1,56 @@
+var socket = io("http://localhost:8080");
+
+socket.on("disconnect", () => {
+
+    console.log("disconectou")
+
+});
+
+    
+    var usernameField = document.getElementById("username")
+    usernameField.value = localStorage.getItem("key")
+
+
+socket.on("showmsg", (data) => {
+        console.log(data)
+        var chat = document.getElementById("chat")
+        var p = document.createElement("p")
+        p.innerHTML = data.username + " : " + data.msg
+        chat.append(p)
+    })
+
+   
+
+function enviar(){
+    var msgField = document.getElementById("msg")
+
+
+    var msg = msgField.value
+    var username = usernameField.value
+
+    socket.emit("msg", {msg: msg, username: username})
+   
+
+}
+
+
+//Evento do onLoad no body
+
+function evento(){
+
+    Swal.fire({
+        title: 'Converse tranquilo e respeite a todos.',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff url(/images/trees.png)',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      })
+
+}
+
