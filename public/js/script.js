8b549f3a0f5333cfba9448ce55438fee4567c64f
remix-pc <guimoreiramanow.contato@gmail.com>
2023-03-10 11:21:30 -0300

    Adicionado REGEX para detectar quando for imagem.
    Agora é possível enviar o link de uma imagem no final contendo .formato da imagem e ela vai aparecer no chat.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
-var socket = io("https://teste-chat.onrender.com", { transports: ['websocket'] })
+var socket = io('http://localhost:4004')
 
-//('http://localhost:4004')
+//("https://teste-chat.onrender.com", { transports: ['websocket'] })
 
 socket.on("disconnect", () => {
 
@@ -8,6 +8,11 @@ socket.on("disconnect", () => {
 
 });
 
+    //Icones provisórios
+    // var imgs = [
+    //      '../images/potatoGlados.png',
+    //      '../images/pinguim.jpg',
+    // ]
     
     var usernameField = document.getElementById("username")
     usernameField.value = localStorage.getItem("key")
@@ -15,16 +20,44 @@ socket.on("disconnect", () => {
 
 socket.on("showmsg", (data) => {
 
+        
+
         console.log(data)
         var chat = document.getElementById("chat")
         var img = document.createElement("img")
+        img.classList.add("perfil")
         var p = document.createElement("p")
         var div = document.createElement('div')
-        img.src = "https://i.pinimg.com/originals/89/5d/79/895d79a1776bfc0de2ceeccc494274d3.png" //../images/pinguim.jpg
-        div.append(img)
-        p.innerHTML = data.username + " : " + data.msg
-        div.append(p)
-        chat.append(div)
+        var image = document.createElement("img")
+        const imgRegex = /\.(gif|jpe?g|png)$/i
+        var teste = data.msg
+        if(imgRegex.test(teste)){
+            console.log("A url é uma imagem")
+            div.append(img)
+            p.innerHTML = data.username + ": "
+            div.append(p)
+            chat.append(div)
+            image.src = teste
+            chat.append(image)
+        }else {
+            div.append(img)
+            p.innerHTML = data.username + ": " + data.msg
+            div.append(p)
+            chat.append(div)
+            image.src = teste
+        }
+
+        
+
+        if(data.username == "Remixo"){
+            img.src = "https://i.pinimg.com/originals/89/5d/79/895d79a1776bfc0de2ceeccc494274d3.png"
+        }else{
+            // let i = Math.random() * 2
+            // let y = parseInt(i)
+            img.src = "../images/duck.png"
+        }
+       
+        
         
     })
 
@@ -40,6 +73,7 @@ function enviar(){
         var username = usernameField.value
         socket.emit("msg", {msg: msg, username: username})
     }
+    msgField.value = ""
 }
 
 
